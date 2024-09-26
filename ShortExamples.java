a4c3543d986a82e6592f0086d91bbd91966675c0
Bharatipatgar <bharatipatgar119@gmail.com>
2024-09-26 19:29:12 +0530

    java

---
+++ ShortExamples.java
@@ -0,0 +1,31 @@
+public class ShortExamples {
+    public static void main(String[] args) {
+                short age = 25;
+        
+                short year = 2024;
+        
+                short temperature = -15;
+        
+                short score = 32000;
+        
+        
+                short height = 175;
+        
+        
+                short distance = 500;
+        
+                short population = 15000;
+        
+        
+                short depth = -32768;
+        
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Score: " + score);
+        System.out.println("Height: " + height);
+        System.out.println("Distance: " + distance);
+        System.out.println("Population: " + population);
+        System.out.println("Depth: " + depth);
+    }
+}
