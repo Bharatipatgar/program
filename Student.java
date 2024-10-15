4484b00e1d9821f6b70b522668feb58815817bd4
Bharatipatgar <bharatipatgar119@gmail.com>
2024-10-15 23:31:21 +0530

    student

---
+++ Student.java
@@ -0,0 +1,40 @@
+public class Student {
+    String name;
+    int age;
+    String studentID;
+    String gradeLevel;
+    String major;
+
+    
+    public Student() {
+        System.out.println("No parameterized constructor");
+    }
+
+    
+    public Student(String name, int age, String studentID, String gradeLevel, String major) {
+        this.name = name;
+        this.age = age;
+        this.studentID = studentID;
+        this.gradeLevel = gradeLevel;
+        this.major = major;
+    }
+
+    
+    public void study() {
+        System.out.println(name + " is studying.");
+    }
+
+    
+    public void takeExam() {
+        System.out.println(name + " is taking an exam.");
+    }
+
+    
+    public void StudentInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Grade Level: " + gradeLevel);
+        System.out.println("Major: " + major);
+    }
+}
